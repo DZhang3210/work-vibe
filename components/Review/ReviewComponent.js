37495590e7909fa4f6a952e184840a9de7d7b8fd
DZhang3210 <53838139+DZhang3210@users.noreply.github.com>
2024-07-18 15:41:47 -0400

    new commit

--- components/Review/ReviewComponent.js
+++ components/Review/ReviewComponent.js
@@ -8,7 +8,7 @@ const ReviewComponent = () => {
             Officevibe is inexpensive, simple to start, and
             easy to use. Your team will thank you for it.
         </div>
-        <div className='grid grid-cols-2 px-2 py-5'>
+        <div className='flex justify-center px-2 py-5'>
             <div className='flex flex-col items-center justify-center gap-4'>
                 <img
                     src = "/brand/brand-stars.svg"
